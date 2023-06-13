8943942eb9d66d9fbbf39ac558c8689ae87fe73b
Maria Grimaldi <maria.grimaldi@edunext.co>
2023-06-13 17:45:35 -0400

    feat: add instructor dashboard filter for limesurvey integration

--- limesurvey/apps.py
+++ limesurvey/apps.py
@@ -10,4 +10,14 @@ class LimeSurveyConfig(AppConfig):
     Configuration for the LimeSurvey Django application.
     """
 
-    name = 'limesurvey'
+    name = "limesurvey"
+    plugin_app = {
+            "settings_config": {
+                "lms.djangoapp": {
+                    "common": {"relative_path": "settings.common"},
+                },
+                "cms.djangoapp": {
+                    "common": {"relative_path": "settings.common"},
+            },
+        }
+    }
