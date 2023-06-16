143b94e66eeb2ff9b777503849bf123bb6e55e95
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-06-16 12:05:07 -0500

    feat: add error message field to display errors returned by the API

--- limesurvey/limesurvey.py
+++ limesurvey/limesurvey.py
@@ -1,5 +1,5 @@
 """TO-DO: Write a description of what this XBlock is."""
-
+from __future__ import annotations
 import pkg_resources
 import requests
 from django.utils import translation
@@ -48,6 +48,12 @@ class LimeSurveyXBlock(XBlock):
         help="The access code of the user for the survey",
     )
 
+    error_message = String(
+        default=None,
+        scope=Scope.user_state,
+        help="The error message to display to the user",
+    )
+
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -65,12 +71,13 @@ class LimeSurveyXBlock(XBlock):
 
         if limesurvey_api_url:
             anonymous_user_id = self.runtime.anonymous_student_id
-            if not self.user_in_survey(anonymous_user_id):
-                self.add_participant_to_survey(
-                    self.runtime.get_real_user(anonymous_user_id),
-                    anonymous_user_id,
+            if response := self.valid_session_key(anonymous_user_id) is not False:
+                if not self.user_in_survey(response):
+                    self.add_participant_to_survey(
+                        self.runtime.get_real_user(anonymous_user_id),
+                        anonymous_user_id,
                 )
-            self.get_survey(anonymous_user_id)
+                self.get_survey(anonymous_user_id)
 
         html = self.resource_string("static/html/limesurvey.html")
         frag = Fragment(html.format(self=self))
@@ -131,9 +138,24 @@ class LimeSurveyXBlock(XBlock):
         self.survey_url = f"{settings.LIMESURVEY_URL}/{self.survey_id}"
         self.access_code = self.get_student_access_code(anonymous_user_id)
 
-    def user_in_survey(self, anonymous_user_id: str) -> bool:
+    def user_in_survey(self, response: dict) -> bool:
         """
         Check if the user is already in the survey.
+
+        args:
+            response (dict): The response from the API
+
+        Returns:
+            bool: True if the user is in the survey, False otherwise
+        """
+        if isinstance(response.get("result"), list):
+            return len(response.get("result", 0)) > 0
+
+        return False
+
+    def valid_session_key(self, anonymous_user_id: str) -> bool | dict:
+        """
+        Check if the session key is valid.
 
         Args:
             anonymous_user_id (str): The anonymous user ID of the user
@@ -143,12 +165,7 @@ class LimeSurveyXBlock(XBlock):
             ["attribute_1"], {"attribute_1": anonymous_user_id}
         ]
 
-        response = self._invoke("list_participants", *params)
-
-        if isinstance(response.get("result"), list):
-            return len(response.get("result")) > 0
-
-        return False
+        return self._invoke("list_participants", *params)
 
     def get_student_access_code(self, anonymous_user_id):
         """
@@ -167,7 +184,7 @@ class LimeSurveyXBlock(XBlock):
             {"attribute_1": anonymous_user_id}
         )
 
-        return response.get("result").get("token")
+        return response.get("result", {}).get("token")
 
     def add_participant_to_survey(self, user, anonymous_user_id: str):
         """
@@ -195,7 +212,7 @@ class LimeSurveyXBlock(XBlock):
 
         self._invoke("add_participants", self.survey_id, [participant])
 
-    def _invoke(self, method: str, *params) -> dict:
+    def _invoke(self, method: str, *params) -> dict | bool:
         """
         Invoke a method on the LimeSurvey API.
 
@@ -221,7 +238,13 @@ class LimeSurveyXBlock(XBlock):
         if response.status_code != requests.status_codes.codes.ok: # pylint: disable=no-member
             raise Exception(response.text)
 
-        return response.json()
+        json_response = response.json()
+
+        if "status" in json_response.get("result", {}):
+            self.error_message = json_response.get("result").get("status")
+            return False
+
+        return json_response
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
