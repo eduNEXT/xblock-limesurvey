f222211dd78b59397ee01d05fafb61c960cb63fc
Maria Grimaldi <maria.grimaldi@edunext.co>
2023-06-21 15:24:45 -0400

    feat: add instructor dashboard filter for limesurvey integration (#8)

---
+++ limesurvey/settings/common.py
@@ -0,0 +1,11 @@
+"""
+Settings for the LimeSurvey plugin.
+"""
+from limesurvey import LIMESURVEY_ROOT_DIRECTORY
+
+
+def plugin_settings(settings):
+    """
+    Read / Update necessary project settings.
+    """
+    settings.MAKO_TEMPLATE_DIRS_BASE.append(LIMESURVEY_ROOT_DIRECTORY / "templates")
