5e67c807175723cc677841e8614ecb82b4fd115f
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-06-22 11:41:22 -0500

    feat: add api credentials in settings

--- limesurvey/settings/common.py
+++ limesurvey/settings/common.py
@@ -10,3 +10,12 @@ def plugin_settings(settings):
     """
     settings.MAKO_TEMPLATE_DIRS_BASE.append(LIMESURVEY_ROOT_DIRECTORY / "templates")
     settings.LIMESURVEY_API_TIMEOUT = 5 # average timeout for regular HTTP request
+
+    settings.LIMESURVEY_API_USER = getattr(settings, 'ENV_TOKENS', {}).get(
+        'LIMESURVEY_API_USER',
+        settings.LIMESURVEY_API_USER
+    )
+    settings.LIMESURVEY_API_PASSWORD = getattr(settings, 'ENV_TOKENS', {}).get(
+        'LIMESURVEY_API_PASSWORD',
+        settings.LIMESURVEY_API_PASSWORD
+    )
