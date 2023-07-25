4aa17e31a2bba3bb293b876c26421151da833c89
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-07-25 11:24:25 -0500

    fix: load paragon styles in instructor dashboard

--- limesurvey/static/js/src/instructor.js
+++ limesurvey/static/js/src/instructor.js
@@ -1,14 +1,33 @@
-/* Javascript for LimeSurveyTab. */
-function LimeSurveyTab(runtime, element) {
+$(function () {
 
-    $(function ($) {
-        /*
-        Use `gettext` provided by django-statici18n for static translations
+    const cssUrl = "https://cdn.jsdelivr.net/npm/@edx/paragon@20.45.5/dist/paragon.min.css";
 
-        var gettext = LimeSurveyXBlocki18n.gettext;
-        */
+    $('<link>').attr({
+        href: cssUrl,
+        rel: "stylesheet"
+    }).appendTo('head');
 
-        /* Here's where you'd do things on page load. */
-        console.log("LimeSurveyTab");
-    });
-}
+    $('.limesurvey-instructor-wrapper').hide();
+    $('.admin-panel').click(function (e) {
+        e.preventDefault();
+        $('.limesurvey-instructor-wrapper').show();
+        const url = $(this).attr('href');
+        $('.limesurvey-instructor-wrapper').append(`
+            <a href="" class="go-back">← Back</a>
+            <div>
+                <iframe
+                    class="limesurvey-admin-iframe"
+                    frameborder="0"
+                    src="${url}">
+                </iframe>
+            </div>
+        `);
+        $('.paragon-styles').hide();
+        $('.go-back').click(function (e) {
+            e.preventDefault();
+            $('.paragon-styles').show();
+            $('.limesurvey-instructor-wrapper').html('');
+            $(this).hide();
+        });
+    })
+});
