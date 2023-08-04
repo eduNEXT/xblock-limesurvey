7b6ce52ed4fa6c7cb02174b7368d86676326dde7
Maria Grimaldi <maria.grimaldi@edunext.co>
2023-08-04 15:31:10 -0400

    docs: update changelog entry

--- limesurvey/__init__.py
+++ limesurvey/__init__.py
@@ -5,6 +5,6 @@ import os
 from pathlib import Path
 from .limesurvey import LimeSurveyXBlock
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 LIMESURVEY_ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
